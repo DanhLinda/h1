4865d62cfa7f6eb7aecc8da17d095c56880512a8
DanhLinda <76256554+DanhLinda@users.noreply.github.com>
2021-01-10 21:40:42 -0500

    wk2 day 2

---
+++ Homeworkwk2day2/src/question2/student.java
@@ -0,0 +1,14 @@
+package question2;
+
+public class student {
+
+		public String name = "linda";
+		public int age = 18;
+
+		public void display() {
+			System.out.print(name + age);
+		}
+	}
+
+
+ 
---
+++ Homeworkwk2day2/src/question2/studentinformation.java
@@ -0,0 +1,13 @@
+package question2;
+
+public class studentinformation {
+
+	public static void main(String[] args) {
+	student obj1 = new student(); 
+	System.out.println(obj1.age);
+	
+	System.out.println(obj1.name);
+	}
+
+
+}
